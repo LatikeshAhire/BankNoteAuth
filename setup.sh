5ae7ddc41779dd5d6db429629e9ebde0ff2f9d12
latikesh27 <68605742+latikesh27@users.noreply.github.com>
2021-10-04 20:22:42 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-mkdir -p ~/.streamlitecho "[server]
-headless = true
-port = $PORT
-enableCORS = false
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
